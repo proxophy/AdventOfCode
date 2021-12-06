217c798788dfc7adeb88c7da70358b61f8e36db4
Isabel T H <40540780+proxophy@users.noreply.github.com>
2021-12-06 14:13:08 +0100

    Day 5 and 6

---
+++ aoc2021/src/aoc2021/Day5.java
@@ -0,0 +1,148 @@
+package aoc2021;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Day5 implements AoCTask {
+    Segment[] segments;
+    int maxx = 0;
+    int maxy = 0;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scr;
+
+        File file = new File("input_s");
+        scr = new Scanner(file);
+        Day5 small = new Day5();
+        small.readInput(scr, 10);
+        System.out.println(small.task2());
+        scr.close();
+
+        File file2 = new File("input");
+        scr = new Scanner(file2);
+        Day5 big = new Day5();
+        big.readInput(scr, 500);
+        scr.close();
+        System.out.println(big.task2());
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++) {
+            big.task1();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total execution time: " + (endTime - startTime) / 10);
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++) {
+            big.task2();
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Total execution time: " + (endTime - startTime) / 10);
+
+    }
+
+    @Override
+    public void readInput(Scanner scr, int n) {
+        this.segments = new Segment[n];
+        for (int i = 0; i < n; i++) {
+            String line = scr.nextLine();
+            line = line.replace(',', ' ');
+            Scanner lscr = new Scanner(line);
+            int x1 = lscr.nextInt();
+            int y1 = lscr.nextInt();
+            lscr.next();
+            int x2 = lscr.nextInt();
+            int y2 = lscr.nextInt();
+            Segment seg = new Segment(x1, y1, x2, y2);
+            lscr.close();
+            segments[i] = seg;
+
+            maxx = Math.max(maxx, Math.max(x1, x2));
+            maxy = Math.max(maxy, Math.max(y1, y2));
+        }
+        // System.out.println(maxx + " " + maxy);
+    }
+
+    @Override
+    public int task1() {
+        int[][] grid = new int[this.maxy + 1][this.maxx + 1];
+        for (Segment seg : this.segments) {
+            if (seg.isHorizontal()) {
+                for (int j = Math.min(seg.x1, seg.x2); j <= Math.max(seg.x1, seg.x2); j++) {
+                    grid[seg.y1][j] = grid[seg.y1][j] + 1;
+                }
+            } else if (seg.isVertical()) {
+                for (int i = Math.min(seg.y1, seg.y2); i <= Math.max(seg.y1, seg.y2); i++) {
+                    grid[i][seg.x1] = grid[i][seg.x1] + 1;
+                }
+            }
+        }
+        int c = 0;
+        for (int i = 0; i < this.maxy + 1; i++) {
+            for (int j = 0; j < this.maxx + 1; j++) {
+                if (grid[i][j] > 1)
+                    c++;
+            }
+        }
+
+        return c;
+    }
+
+    @Override
+    public int task2() {
+        int[][] grid = new int[this.maxy + 1][this.maxx + 1];
+        for (Segment seg : this.segments) {
+            int xup = seg.x2 != seg.x1 ? (seg.x2 - seg.x1) / (Math.abs(seg.x2 - seg.x1)) : 0;
+            int yup = seg.y2 != seg.y1 ? (seg.y2 - seg.y1) / (Math.abs(seg.y2 - seg.y1)) : 0;
+
+            int x = seg.x1, y = seg.y1;
+            while (x != seg.x2 || y != seg.y2) {
+                grid[y][x]++;
+                x += xup;
+                y += yup;
+            }
+
+            grid[seg.y2][seg.x2]++;
+        }
+        int c = 0;
+        for (int i = 0; i < this.maxy + 1; i++) {
+            for (int j = 0; j < this.maxx + 1; j++) {
+                if (grid[i][j] > 1)
+                    c++;
+            }
+        }
+
+        return c;
+    }
+
+}
+
+class Segment {
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+
+    Segment(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    boolean isHorizontal() {
+        return y1 == y2;
+    }
+
+    boolean isVertical() {
+        return x1 == x2;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x1 + ", " + y1 + ") -> " + "(" + x2 + ", " + y2 + ")";
+
+    }
+}
---
+++ aoc2021/src/aoc2021/Day6.java
@@ -0,0 +1,110 @@
+package aoc2021;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Day6 {
+    ArrayList<Fish> fish = new ArrayList<Fish>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scr;
+
+        File file = new File("input_s");
+        scr = new Scanner(file);
+        Day6 small = new Day6();
+        small.readInput(scr, 0);
+        System.out.println(small.task2_l());
+        scr.close();
+
+        File file2 = new File("input");
+        scr = new Scanner(file2);
+        Day6 big = new Day6();
+        big.readInput(scr, 0);
+        scr.close();
+        System.out.println(big.task2_l());
+    }
+
+    public void readInput(Scanner scr, int n) {
+        String line = scr.nextLine();
+        String[] args = line.split(",");
+        for (String s : args) {
+            int val = Integer.parseInt(s);
+            fish.add(new Fish(val));
+        }
+
+    }
+
+    public int task1() {
+        for (int i = 1; i <= 80; i++) {
+            ArrayList<Fish> new_fish = new ArrayList<Fish>();
+            new_fish.addAll(fish);
+            for (Fish f : fish) {
+                if (f.haveBaby()) { // new fish
+                    new_fish.add(new Fish(8));
+                }
+
+                f.set();
+            }
+            fish = new_fish;
+
+        }
+
+        return fish.size();
+    }
+
+    public Long task2() {
+        // much faster than task 1 version, thanks to DP
+
+        // nums[i] describes how many fish have internal timer i
+        long[] nums = new long[9];
+        // initialize
+        for (Fish f : fish) {
+            nums[f.dleft]++;
+        }
+        // simulate
+        for (int j = 1; j <= 256; j++) {
+            long[] old_nums = Arrays.copyOf(nums, nums.length);
+            for (int i = 0; i < 8; i++) {
+                nums[i] = old_nums[i + 1];
+            }
+            nums[6] += old_nums[0];
+            nums[8] = old_nums[0];
+        }
+
+        long sum = 0;
+        for (int i = 0; i < 9; i++) {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+
+}
+
+class Fish {
+    int dleft;
+
+    Fish(int l) {
+        this.dleft = l;
+    }
+
+    @Override
+    public String toString() {
+        return "" + dleft;
+    }
+
+    void set() {
+        if (dleft == 0) {
+            dleft = 6;
+        } else {
+            dleft--;
+        }
+    }
+
+    boolean haveBaby() {
+        return dleft == 0;
+    }
+}
