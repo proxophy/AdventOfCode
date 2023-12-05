a69c12aa3912fb14983c778fc3ba46a281a757d4
Isabel <isabelthaas@gmail.com>
2023-12-05 16:51:13 +0100

    Day 5

--- aoc2023/day5.py
+++ aoc2023/day5.py
@@ -7,9 +7,7 @@ from utils import parse_intlist
 class Day5:
     def __init__(self, inp: str) -> None:
         parts = inp.split("\n\n")
-
         self.seeds = parse_intlist(parts[0].split(":")[1])
-
         self.rmaps = [self.parse_map(part)for part in parts[1:]]
         return
 
@@ -21,19 +19,15 @@ class Day5:
             ranges[(ss, ss+rl-1)] = ds
         return ranges
 
-    def map_num(self, rmap: Dict, num: int):
+    def map_num(self, num: int, rmap: Dict):
         for (ss, se) in rmap:
             if ss <= num <= se:
                 return (num - ss) + rmap[(ss, se)]
         return num
 
     def task1(self):
-        arr = []
-        for seed in self.seeds:
-            curr = seed
-            for rmap in self.rmaps:
-                curr = self.map_num(rmap, curr)
-            arr.append(curr)
+        arr = [functools.reduce(self.map_num, self.rmaps, seed)
+               for seed in self.seeds]
         return min(arr)
 
     def split_range(self, rmap: Dict, start: int, end: int):
@@ -60,13 +54,12 @@ class Day5:
         # map ranges
         arr = []
         for ss, se in splits:
-            arr.append((self.map_num(rmap, ss),
-                       self.map_num(rmap, se)))
+            arr.append((self.map_num(ss, rmap),
+                       self.map_num(se, rmap)))
 
         return arr
 
     def task2(self):
-        i = 0
         arr = []
 
         intervals = [(self.seeds[i], self.seeds[i] + self.seeds[i+1] - 1)
@@ -79,3 +72,13 @@ class Day5:
         arr, _ = zip(*intervals)
 
         return min(arr)
+
+
+if __name__ == "__main__":
+    def foo(y: int, x: str) -> int:
+        return int(x)+y
+    l = "1", "2", "3", "4", "5", "6"
+    res = functools.reduce(foo, l, 1)
+    print(res)
+
+    pass
